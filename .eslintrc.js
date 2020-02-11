bb5e1e04819c0f4d3ff7d93fade4c8d00df940de
tukno <oliveiradavid007@gmail.com>
2020-02-11 01:53:11 -0400

    Linter, formatter and hot reload configurations.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,18 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base, plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['prettier'],
   rules: {
+    'prettier/prettier': 'error',
   },
 };
